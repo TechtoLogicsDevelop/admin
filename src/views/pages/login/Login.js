3c50485cf4671b2be013418eaa9e820ef558a435
techtologics-dev <81003887+techtologics-dev@users.noreply.github.com>
2021-05-28 09:31:58 +0530

    Add files via upload

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -12,6 +12,7 @@ import {
   CInputGroup,
   CInputGroupPrepend,
   CInputGroupText,
+  CHeaderNavLink,
   CRow
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
@@ -25,9 +26,17 @@ const Login = () => {
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
+				
                   <CForm>
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
+				   <CCard color="white" className="text-white text-center">
+            <CCardBody>
+             
+          <img src={'avatars/main-logo-black.png'}   />
+       
+            </CCardBody>
+          </CCard>
+                    <h1 className=" text-center">Login To Admin Panel</h1>
+                    <p className="text-muted text-center">Sign In to your account</p>
                     <CInputGroup className="mb-3">
                       <CInputGroupPrepend>
                         <CInputGroupText>
@@ -45,28 +54,17 @@ const Login = () => {
                       <CInput type="password" placeholder="Password" autoComplete="current-password" />
                     </CInputGroup>
                     <CRow>
-                      <CCol xs="6">
-                        <CButton color="primary" className="px-4">Login</CButton>
+                      <CCol xs="6" >
+                        <CButton className="text-center" color="success" className="px-4 ">Login</CButton>
                       </CCol>
                       <CCol xs="6" className="text-right">
-                        <CButton color="link" className="px-0">Forgot password?</CButton>
+                        <CButton color="link" className="px-0"><CHeaderNavLink to="">Forgot password? </CHeaderNavLink></CButton>  
                       </CCol>
                     </CRow>
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                      labore et dolore magna aliqua.</p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>Register Now!</CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
+             
             </CCardGroup>
           </CCol>
         </CRow>
