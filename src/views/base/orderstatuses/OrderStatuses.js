a35ae99ce3108769b1abb29488804469e11a6f0b
techtologics-dev <81003887+techtologics-dev@users.noreply.github.com>
2021-06-08 09:42:59 +0530

    Add files via upload

---
+++ src/views/base/orderstatuses/OrderStatuses.js
@@ -0,0 +1,145 @@
+import React from 'react'
+import {
+  CBadge,
+   CHeaderNav,
+  CHeaderNavItem,
+  CHeaderNavLink,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CSwitch,
+  CRow
+} from '@coreui/react'
+import { DocsLink } from 'src/reusable'
+
+
+
+const OrderStatuses = () => {
+  return (
+    <>
+     
+      
+
+    
+	   <CRow>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+          OrderStatuses
+            </CCardHeader>
+			
+		   
+            <CCardBody>
+               <CCardHeader>
+            Order Statuses (List)
+			 <CHeaderNavItem  className="px-3" className="text-right">
+          <CHeaderNavLink to="/addnewstatus">Add New Status </CHeaderNavLink>
+        </CHeaderNavItem>
+            </CCardHeader>
+
+              
+
+              <table className="table table-hover table-outline mb-0 d-none d-sm-table">
+                <thead className="thead-light">
+                  <tr>
+                    
+                    <th>  Select</th>
+                    <th>Status Name</th>
+                   
+					<th>Status</th>
+					
+					  <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                   <td>
+                      <CSwitch
+                      className="mr-1"
+                      color="success"
+                      defaultChecked
+                      variant="outline"
+                    />
+                     
+                    </td>
+                    <td>
+                     Pending
+                     
+                    </td>
+					
+					
+						<td>
+					<CSwitch className={'mx-1'} variant={'3d'} color={'primary'} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+					</td>
+					
+					<td>
+					View / Edit / Delete
+					</td>
+                  </tr>
+				   <tr>
+                   <td>
+                      <CSwitch
+                      className="mr-1"
+                      color="success"
+                      defaultChecked
+                      variant="outline"
+                    />
+                     
+                    </td>
+                    <td>
+                     Awaiting Fulfillment
+                     
+                    </td>
+					
+					
+					
+						<td>
+					<CSwitch className={'mx-1'} variant={'3d'} color={'primary'} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+					</td>
+					
+					<td>
+					View / Edit / Delete
+					</td>
+                  </tr>
+				    <tr>
+                   <td>
+                      <CSwitch
+                      className="mr-1"
+                      color="success"
+                      defaultChecked
+                      variant="outline"
+                    />
+                     
+                    </td>
+                    <td>
+                     Awaiting Pickup
+                     
+                    </td>
+				
+					
+					
+						<td>
+					<CSwitch className={'mx-1'} variant={'3d'} color={'primary'} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+					</td>
+					
+					<td>
+					View / Edit / Delete
+					</td>
+                  </tr>
+				   
+				  
+                </tbody>
+              </table>
+
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+        
+    </>
+  )
+}
+
+export default OrderStatuses
