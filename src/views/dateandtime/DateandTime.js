a0594fe429845268a99a6425a186ee8ec85d76cc
techtologics-dev <81003887+techtologics-dev@users.noreply.github.com>
2021-06-08 09:15:33 +0530

    Add files via upload

---
+++ src/views/dateandtime/DateandTime.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CCollapse,
+  CDropdownItem,
+  CDropdownMenu,
+  CDropdownToggle,
+  CFade,
+  CForm,
+  CFormGroup,
+  CFormText,
+  CValidFeedback,
+  CInvalidFeedback,
+  CTextarea,
+  CInput,
+  CInputFile,
+  CInputCheckbox,
+  CInputRadio,
+  CInputGroup,
+  CInputGroupAppend,
+  CInputGroupPrepend,
+  CDropdown,
+  CInputGroupText,
+  CLabel,
+  CSelect,
+  CRow,
+  CPopover,
+  CHeaderNav,
+  CHeaderNavItem,
+  CHeaderNavLink,
+  CSwitch
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { DocsLink } from 'src/reusable'
+
+const DateandTime = () => {
+  const [collapsed, setCollapsed] = React.useState(true)
+  const [showElements, setShowElements] = React.useState(true)
+  
+
+  return (
+    <>
+      
+      <CRow>
+        <CCol xs="12" md="12">
+          <CCard>
+            <CCardHeader>
+              Date and Time Setting
+             
+            </CCardHeader>
+            <CCardBody>
+              <CForm action="" method="post" encType="multipart/form-data" className="form-horizontal">
+			  
+				 <CFormGroup row>
+                  <CCol md="3">
+                    <CLabel htmlFor="select">Your Time Zone</CLabel>
+                  </CCol>
+                  <CCol xs="12" md="9">
+                    <CSelect custom name="select" id="select">
+                      <option value="0">Please select</option>
+                      <option value="1">Time Zone 1</option>
+                      <option value="2">Time Zone 2</option>
+                      <option value="3">Time Zone 3</option>
+                    </CSelect>
+                  </CCol>
+                </CFormGroup>
+				 <CFormGroup row>
+                  <CCol md="3"><CLabel>Enable DST Correction?</CLabel></CCol>
+                  <CCol md="9">
+                    <CFormGroup variant="checkbox" className="checkbox">
+                      <CInputCheckbox 
+                        id="checkbox1" 
+                        name="checkbox1" 
+                        value="option1" 
+                      />
+                      <CLabel variant="checkbox" className="form-check-label" htmlFor="checkbox1"> Yes,Enable Daylight Savings Time Correction</CLabel>
+                    </CFormGroup>
+                   
+                    
+                  </CCol>
+                </CFormGroup>
+				 <CFormGroup row>
+                  <CCol md="3"><CLabel>Display Date Format</CLabel></CCol>
+                 <CCol xs="9" md="9">
+                   <CInput id="text-input" name="text-input" placeholder="DD/MM/YYYY" />
+                  </CCol>
+                </CFormGroup>
+				 <CFormGroup row>
+                  <CCol md="3"><CLabel>Extended Display Date Format</CLabel></CCol>
+                 <CCol xs="9" md="9">
+                   <CInput id="text-input" name="text-input" placeholder="DD/MM/YYYY 00:00:00" />
+                  </CCol>
+                </CFormGroup>
+				 
+				
+              </CForm>
+            </CCardBody>
+            <CCardFooter>
+              <CButton type="submit" size="sm" color="primary"><CIcon name="cil-scrubber" /> Submit</CButton>
+              <CButton type="reset" size="sm" color="danger"><CIcon name="cil-ban" /> Reset</CButton>
+            </CCardFooter>
+          </CCard>
+         
+        </CCol>
+       
+      </CRow>
+     
+     
+     
+     
+     
+    </>
+  )
+}
+
+export default DateandTime
